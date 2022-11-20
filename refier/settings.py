5905125ceec2f61ee8a118b9283f1f8d7602ecfe
Jash <jashsharma619@gmail.com>
2022-11-21 02:06:44 +0530

    cors whitelist added

--- refier/settings.py
+++ refier/settings.py
@@ -135,3 +135,9 @@ WEBPACK_LOADER = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'https://refier-api.herokuapp.com'
+]
