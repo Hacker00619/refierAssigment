2123feef54d24ebfaeabec4eb112a784241cf92d
Jash <jashsharma619@gmail.com>
2022-11-21 01:04:00 +0530

    api url added

--- jobsite/urls.py
+++ jobsite/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from jobsite.views import JobViewSet
 from rest_framework import routers
+from jobsite import views
 
 router = routers.DefaultRouter()
 router.register(r'jobs', JobViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    # path('', views.index, name='index'),
+    path('api/', include(router.urls))
 ]
