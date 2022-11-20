0da643cb5215c49efe4084fdc54aaf4b4c01475a
Jash <jashsharma619@gmail.com>
2022-11-21 03:03:26 +0530

    settings.py

--- refier/settings.py
+++ refier/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = '/build/'
+STATIC_ROOT = 'build/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,
                     os.path.join(BASE_DIR, 'build/static'),
