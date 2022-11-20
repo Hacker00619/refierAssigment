698f89a2c5b3a0da8d81a6901d8de5df19499c71
Jash <jashsharma619@gmail.com>
2022-11-21 01:37:55 +0530

    import fixed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import JobsPage from "./pages/JobsPage";
-import Header from "./components/Header";
-import PostJobPage from "./pages/PostJobPage";
-import EditJobPage from "./pages/EditJobPage";
+import JobsPage from "./pages/JobsPage.jsx";
+import Header from "./components/Header.jsx";
+import PostJobPage from "./pages/PostJobPage.jsx";
+import EditJobPage from "./pages/EditJobPage.jsx";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<JobsPage />} />
-        <Route path="/post-job" element={<PostJobPage />} />
-        <Route path="/edit-job/:id" element={<EditJobPage />} />
-      </Routes>
+        <Route path="/" element={<JobsPage />} />{" "}
+        <Route path="/post-job" element={<PostJobPage />} />{" "}
+        <Route path="/edit-job/:id" element={<EditJobPage />} />{" "}
+      </Routes>{" "}
     </Router>
   );
 }
