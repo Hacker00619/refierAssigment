4237bbae387e21743c78644ee79892222c4c2047
Jash <jashsharma619@gmail.com>
2022-11-21 01:47:15 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import JobsPage from "./pages/JobsPage.jsx";
+import JobsPage from "./Pages/JobsPage.jsx";
 import Header from "./components/Header.jsx";
-import PostJobPage from "./pages/PostJobPage.jsx";
-import EditJobPage from "./pages/EditJobPage.jsx";
+import PostJobPage from "./Pages/PostJobPage.jsx";
+import EditJobPage from "./Pages/EditJobPage.jsx";
 
 function App() {
   return (
