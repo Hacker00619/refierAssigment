ca0640cc15e57a8b831bcfe1f5ebfe31fbed1cb5
Jash <jashsharma619@gmail.com>
2022-11-21 02:23:03 +0530

    minor fix

--- refier/wsgi.py
+++ refier/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jxsrmaweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'refier.settings')
 
 application = get_wsgi_application()
